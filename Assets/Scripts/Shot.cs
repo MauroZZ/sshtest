244ef636ed7787cdf3af4b3d5729fea11b5c7509
Jonnathan Riffo <jonnathan.riffo@virginiogomez.cl>
2025-04-12 21:59:34 -0400

    Funcion de disparo del tanque hecha

---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawn;
+
+    public float shotForce = 1500;
+    public float shotRate = 0.5F;
+
+    private float shotRateTime = 0;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Time.time > shotRateTime)
+            {
+                GameObject newBullet;
+
+                newBullet = Instantiate(bullet, spawn.position, spawn.rotation);
+                newBullet.GetComponent<Rigidbody>().AddForce(spawn.forward * shotForce);
+
+                shotRateTime = Time.time + shotRate;
+
+                Destroy(newBullet, 2);
+            }
+        }
+    }
+}
